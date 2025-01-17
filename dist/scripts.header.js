cf5ec372ec78aa9e2c9cc0bd71c0fa59953f8e69
Tonda (Tomáš) Kalina <kalina@shoptet.cz>
2025-01-17 10:06:15 +0100

    add customizable snowflake generation and styling

--- dist/scripts.header.js
+++ dist/scripts.header.js
@@ -16,7 +16,7 @@
   \******************************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\nconsole.log('Example script header');\n\n//# sourceURL=webpack://vodazivoda-ext/./src/header/script.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nfunction getSnowflakeCount() {\n  const month = new Date().getMonth(); // Získání aktuálního měsíce (0 = leden, 1 = únor, ..., 11 = prosinec)\n\n  // Definování množství vloček podle měsíce\n  const snowfallMap = [200,\n  // Leden\n  300,\n  // Únor (nejvíce chumelí)\n  250,\n  // Březen\n  100,\n  // Duben\n  0,\n  // Květen (žádný sníh)\n  0,\n  // Červen\n  0,\n  // Červenec\n  0,\n  // Srpen\n  0,\n  // Září\n  50,\n  // Říjen (začátek sněhu)\n  150,\n  // Listopad\n  250 // Prosinec\n  ];\n  return snowfallMap[month]; // Vrátí počet vloček podle aktuálního měsíce\n}\nfunction createSnowflakes(count) {\n  const container = document.querySelector(\".snowflakes\");\n  for (let i = 0; i < count; i++) {\n    let snowflake = document.createElement(\"div\");\n    snowflake.classList.add(\"snowflake\");\n    let inner = document.createElement(\"div\");\n    inner.classList.add(\"inner\");\n    inner.innerHTML = \"❅\";\n    snowflake.style.left = Math.random() * 100 + \"%\";\n    snowflake.style.animationDelay = Math.random() * 3 + \"s\";\n    inner.style.animationDelay = Math.random() * 10 + \"s\";\n    snowflake.appendChild(inner);\n    container.appendChild(snowflake);\n  }\n}\nconst snowflakeCount = getSnowflakeCount();\ncreateSnowflakes(snowflakeCount);\n\n//# sourceURL=webpack://vodazivoda-ext/./src/header/script.js?");
 
 /***/ })
 
